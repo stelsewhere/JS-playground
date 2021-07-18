ec3bf6df457cf94720ebc552056005236b6ffdaf
Liudmyla Kladko <86486525+stelsewhere@users.noreply.github.com>
2021-07-18 16:28:12 +0300

    Add files via upload

--- Excersises.js
+++ Excersises.js
@@ -76,3 +76,15 @@ function ab(text) {
 }
 
 console.log(ab("webmaster"))
+
+// Write a JavaScript program to display the current day and time in the following format.
+// Sample Output : Today is : Tuesday. Current time is : 10 PM : 30 : 38
+var today = new Date();
+var day = today.getDay();
+var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log("Today is: " + dayList[day] + ".");
+var hour = today.getHours()
+var minute = today.getMinutes()
+var second = today.getSeconds()
+var prepand = (hour >= 12 ? "PM" : "AM")
+console.log("Current time is: " + hour + prepand + " : " + minute + " : " + second)
