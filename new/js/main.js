1da2157d426d5414bae9c6820974ad26426ecde6
Liudmyla Kladko <86486525+stelsewhere@users.noreply.github.com>
2021-07-21 19:12:35 +0300

    Add files via upload

--- new/js/main.js
+++ new/js/main.js
@@ -1,26 +1,28 @@
 // Search
 
 var search = function() {
-    var isSearchActive = true;
-
-    if (isSearchActive) {
-        document.getElementById("show").classList.toggle("show-search"); 
-        document.getElementById('searchButton').classList.toggle('close-icon');
-    } 
+    document.getElementById("show").classList.toggle("show-search"); 
+    document.getElementById('searchButton').classList.toggle('close-icon');
 }
 
-// footer
-let showList = document.querySelector('.footer__top');
+// footer 
+/*
+let showList = document.querySelectorAll('.show-list');
 
 showList.onclick = function(event) {
     var target = event.target
 
     document.getElementById('list').classList.toggle('show')
     document.getElementById('show-list').classList.toggle('expanded')
+} */
 
-}
-    
 
+document.querySelectorAll('.footer__title').forEach(item => {
+    item.addEventListener('click', event => {
+        document.getElementById('list').classList.toggle('show')
+        document.getElementById('show-list').classList.toggle('expanded')
+    })
+})
 
 
 
