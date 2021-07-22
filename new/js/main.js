f8e18146bda1a587723849b7b979fb7db77b160f
Liudmyla Kladko <86486525+stelsewhere@users.noreply.github.com>
2021-07-22 14:53:57 +0300

    Add files via upload

--- new/js/main.js
+++ new/js/main.js
@@ -1,3 +1,15 @@
+
+// Menu
+
+function openNav() {
+    document.getElementById('overlay').style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById('overlay').style.width = "0%";
+}
+
+
 // Search
 
 var search = function() {
@@ -5,26 +17,53 @@ var search = function() {
     document.getElementById('searchButton').classList.toggle('close-icon');
 }
 
+
 // footer 
-/*
-let showList = document.querySelectorAll('.show-list');
 
-showList.onclick = function(event) {
-    var target = event.target
+var showList = document.getElementsByClassName("footer__title");
+var i;
 
-    document.getElementById('list').classList.toggle('show')
-    document.getElementById('show-list').classList.toggle('expanded')
-} */
+for (i = 0; i < showList.length; i++) {
+  showList[i].addEventListener("click", function() {
+    this.classList.toggle("show");
+    this.classList.toggle('expanded');
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+      
+    }
+  });
+}
 
 
-document.querySelectorAll('.footer__title').forEach(item => {
-    item.addEventListener('click', event => {
-        document.getElementById('list').classList.toggle('show')
-        document.getElementById('show-list').classList.toggle('expanded')
-    })
+// Menu navigation
+
+document.querySelectorAll('.nav__item--level-1').forEach(item => {
+	item.addEventListener('click', event => {
+		document.getElementById('level2').style.display = "block"
+    document.getElementById('level1').style.display = "none"
+	})
+})
+
+document.querySelectorAll('.nav__item--level-2').forEach(item => {
+	item.addEventListener('click', event => {
+		document.getElementById('level3').style.display = "block"
+    document.getElementById('level2').style.display = "none"
+	})
 })
 
 
+var backToLevelOne = document.querySelector('.caption-level-2');
+var backToLevelTwo = document.querySelector('.caption-level-3');
 
+backToLevelOne.onclick = function() {
+  backToLevelOne.classList.toggle('nav-mobile--level-1')
+}
+
+backToLevelTwo.onclick = function() {
+  backToLevelTwo.classList.toggle('nav-mobile--level-2')
+}
 
 
